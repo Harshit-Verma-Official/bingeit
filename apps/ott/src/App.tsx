69b2093c20fa091dc37e44316d849e1a2ccf0006
Harshit Verma <harshitvermaofficial@gmail.com>
2024-09-24 10:12:29 +0530

    chore: removed test code

--- apps/ott/src/App.tsx
+++ apps/ott/src/App.tsx
@@ -2,10 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
 const App = () => {
-  const x = true;
-
-  if (x) return null;
-
   return <RouterProvider router={router} />;
 };
 
