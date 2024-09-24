f6386cb07515e6e4a58b3cbfce5ab38e1def23dd
Harshit Verma <harshitvermaofficial@gmail.com>
2024-09-24 10:06:29 +0530

    feat(lint): added prettier lint and format config

--- apps/ott/src/App.tsx
+++ apps/ott/src/App.tsx
@@ -1,7 +1,11 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 const App = () => {
+  const x = true;
+
+  if (x) return null;
+
   return <RouterProvider router={router} />;
 };
 
